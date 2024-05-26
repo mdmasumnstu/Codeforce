54435b2cc2ec553df8bb27b440a57e9ab973db75
Md. Masum <151396562+mdmasumnstu@users.noreply.github.com>
2024-05-26 12:20:44 +0600

    Add files via upload

---
+++ watermelon.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+
+	int a;
+	cin>>a;
+
+	if(a%2==1)
+		{
+			printf("NO\n");
+		}
+	else
+		{
+ 			if(a==2)
+ 				printf("NO\n");
+ 
+ 			else
+ 			{
+ 				 printf("YES\n");
+ 			}
+		}
+}
