c86f79b8d066758adf144d5338fb102226af7fc1
Md. Masum <151396562+mdmasumnstu@users.noreply.github.com>
2024-06-07 08:04:31 +0600

    Add files via upload

---
+++ Wrong Sunstraction.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,k;
+    cin>>n;cin>>k;
+    
+    if(k==0)
+    {
+        cout<<n<<endl;
+    }
+    
+    for(int i=0;i<k;i++)
+    {
+        if(n%10!=0)
+        {
+            n--;
+        }
+        else
+        {
+            n=n/10;
+        }
+    }
+    cout<<n<<endl;
+
+    return 0;
+}
+
---
+++ square.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+   int a,b,c,d,e,f;
+   cout<< a<< b << c;
+   cin >>a >>b >>c;
+
+   d=sqrt(a);
+   e=sqrt(b);
+   f=sqrt(c);
+
+   cout<<a <<b <<c;
+
+}
+
